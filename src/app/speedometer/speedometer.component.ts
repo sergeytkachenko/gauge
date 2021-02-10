ca9d730b94615c4aca61f51049dde6f55f973d8d
Sergey Tkachenko <s.tkachenko@tscrm.com>
2021-02-11 00:10:27 +0200

    transition

--- src/app/speedometer/speedometer.component.ts
+++ src/app/speedometer/speedometer.component.ts
@@ -7,6 +7,8 @@ interface GaugeSeries {
   to: number;
   startAngle?: number;
   endAngle?: number;
+  x?: number;
+  y?: number;
 }
 
 interface GaugeConfig {
@@ -22,8 +24,8 @@ interface GaugeConfig {
 export class SpeedometerComponent implements OnInit {
 
   private readonly BORDER_PX = 18;
-  private readonly FROM_RADIAN = 1.3;
-  private readonly TO_RADIAN = 2.7;
+  private readonly FROM_RADIAN = -0.70;
+  private readonly TO_RADIAN = 0.70;
   private readonly MARGIN = 0;
 
   @Input()
@@ -67,7 +69,7 @@ export class SpeedometerComponent implements OnInit {
 
   private draw(): void {
     const config = this.config;
-    const pi = 3.14;
+    const pi = Math.PI;
     const heightOrWidth = Math.min(this.width, this.height);
     const outerRadius = heightOrWidth / 2 - this.MARGIN;
     const innerRadius = outerRadius - this.BORDER_PX;
@@ -81,15 +83,18 @@ export class SpeedometerComponent implements OnInit {
     const firstSeries = Array.from(config.series).shift();
     const lastSeries = Array.from(config.series).pop();
     const fromToNumberRange = lastSeries?.to - firstSeries?.from;
-    const onePercentage = (this.TO_RADIAN - this.FROM_RADIAN) / 100;
+    const onePercentage = (Math.abs(this.FROM_RADIAN) + Math.abs(this.TO_RADIAN)) / 100;
     config.series.forEach((series, index) => {
       const prevSeries = config.series[index - 1];
       const startAngle = prevSeries ? prevSeries.endAngle : (this.FROM_RADIAN) * pi;
       const localRange = series.to - series.from;
       const percentage = (localRange / fromToNumberRange) * 100;
       const endAngle = (percentage * onePercentage * pi) + startAngle;
-      series.startAngle = startAngle;
+      series.startAngle = startAngle + 0.02;
       series.endAngle = endAngle;
+      console.log(series.startAngle * 180 / Math.PI);
+      series.x = translateX + Math.cos(series.startAngle * 180 / Math.PI) * outerRadius;
+      series.y = translateY + Math.sin(series.startAngle * 180 / Math.PI) * outerRadius;
     });
     svg
       .selectAll('path')
@@ -97,16 +102,16 @@ export class SpeedometerComponent implements OnInit {
       .enter()
       // tslint:disable-next-line:typedef
       .each(function(series: GaugeSeries) {
+        const d = d3.arc()
+          .innerRadius(innerRadius)
+          .outerRadius(outerRadius)
+          .startAngle(series.startAngle)
+          .endAngle(series.endAngle);
         d3
           .select(this)
           .append('path')
           .attr('transform', translate)
-          .attr('d', d3.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius)
-            .startAngle(series.startAngle)
-            .endAngle(series.endAngle)
-          )
+          .attr('d', d)
           .attr('fill', series.color)
           .attr('opacity', '0')
           .transition()
@@ -114,13 +119,21 @@ export class SpeedometerComponent implements OnInit {
           .attr('opacity', '1');
       })
       // tslint:disable-next-line:typedef
-      .each(function(d) {
+      .each(function(series) {
+        const d = d3.arc()
+          .innerRadius(innerRadius)
+          .outerRadius(outerRadius)
+          .startAngle(series.startAngle)
+          .endAngle(series.endAngle);
+        const centroid = d.centroid(null);
+        const x = centroid[0] + translateX;
+        const y = centroid[1] + translateY;
         d3.select(this)
           .append('text')
-          .attr('x', d.from)
-          .attr('y', d.to)
-          .attr('dy', '0.33em')
-          .text(d.from);
+          .attr('x', x)
+          .attr('y', y)
+          .attr('transform', `translate(${centroid[0]}, ${centroid[1]})`)
+          .text(series.from);
       });
   }
 
